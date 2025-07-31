729c19c0e0f0a3a26c11561d740c0d7803374ca7
rolandaayo <ibiwoyeroland@gmail.com>
2025-07-31 16:12:01 +0100

    new prog

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/rolandayo/Downloads/Others/Node/house-hunt-web/app/layout.tsx
+// File: /Users/rolandayo/Desktop/house-hunt-web/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/rolandayo/Downloads/Others/Node/house-hunt-web/app/page.tsx
+// File: /Users/rolandayo/Desktop/house-hunt-web/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
